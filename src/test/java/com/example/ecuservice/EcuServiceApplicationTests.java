faec164500a66ade4c0b2ca382f592ea707d3e3e
asm <vaskocuturilo@rambler.ru>
2023-03-09 11:27:01 +0100

    DV-00001: Add project.

---
+++ src/test/java/com/example/ecuservice/EcuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcuServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
