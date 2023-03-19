3bb1961348782ecbeef7177d9a0f8ee342381603
asm <vaskocuturilo@rambler.ru>
2023-03-19 12:20:36 +0100

    DV-00001: Add entity.

---
+++ src/main/java/com/example/ecuservice/entity/EcuEntity.java
@@ -0,0 +1,19 @@
+package com.example.ecuservice.entity;
+
+import com.example.ecuservice.enumiration.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "ecu_table")
+public class EcuEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Device id cannot be empty or null")
+    private Long deviceId;
+    private String name;
+    private Status status;
+}
